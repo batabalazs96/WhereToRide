cbf00166723528aa4eb7ff068bb7f088cfbfa8f8
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-03-30 08:46:09 +0200

    seeding desctinations with sample datas

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Destination = require('../models/destination')
+const {descriptors, places} = require('../seeds/seedHelpers')
+const cities = require('../seeds/cities')
+
+mongoose.connect('mongodb://localhost:27017/WhereToRide', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Database connected")
+});
+
+const app = express();
+
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Destination.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+         const random100 = Math.floor(Math.random()*100)
+         const destination = new Destination({
+             title: `${cities[random100].city}`,
+             distance: random100,
+             description: `${sample(descriptors)}`
+            })
+        await destination.save();
+        console.log(destination)
+    }
+}
+seedDB();
+
+

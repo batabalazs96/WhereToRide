bfa23865f9fc3c48b39dc97252b7ba131f7ffdca
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-07-05 13:56:14 +0200

    Add image for destinations

--- models/destination.js
+++ models/destination.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const DestinationSchema = new Schema({
     title:String,
     distance: Number,
+    image: String,
     location: String,
     description: String
 })

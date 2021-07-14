849617e35a5b7586ec344e0f7fe012abe2e40db0
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-07-14 19:31:15 +0200

    adding map controlls

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -5,6 +5,8 @@ style: 'mapbox://styles/mapbox/dark-v10',
 center: [-103.59179687498357, 40.66995747013945],
 zoom: 3
 });
+
+map.addControl(new mapboxgl.NavigationControl());
  
 map.on('load', function () {
 // Add a new source from our GeoJSON data and

71a239f9e6c7c72b737989be1b3b438cc2df6fe0
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-07-14 13:18:33 +0200

    creating a map on a destination

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,9 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: [19.82017, 48.04922], // starting position [lng, lat]
+  center: destination.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
+const  marker1 = new mapboxgl.Marker()
+.setLngLat(destination.geometry.coordinates)
+.addTo(map);

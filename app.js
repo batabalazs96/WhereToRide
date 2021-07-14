09dd51361492b474c4ab3256a05d82423e7d7ed5
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-07-14 20:04:14 +0200

    minor changes for sessions/cookies

--- app.js
+++ app.js
@@ -50,11 +50,13 @@ if (process.env.NODE_ENV !== "production") {
 
 
 const sessionConfig = {
+    name: 'blah',
     secret : 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 *7,
         maxAge: 1000 * 60 * 60 * 24 *7
     }

c4d1a57bedeb25efc91fd7cf825a5fc4a2effe14
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-07-14 09:58:35 +0200

    delete images backend

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -1,4 +1,5 @@
 const Destination = require('../models/destination');
+const cloudinary = require('cloudinary').v2;
 
 module.exports.index = async (req, res) => {
     const destinations = await Destination.find();
@@ -39,6 +40,14 @@ module.exports.updateDestination = async (req, res) => {
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     destination.images.push(...imgs);
     await destination.save();
+    if(req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await destination.updateOne({$pull: {images: {filename:{$in: req.body.deleteImages}}}});
+        console.log(destination)
+    }
+    
     //const destination = await Destination.findByIdAndUpdate(id, req.body.destination);
     req.flash('success', 'Successfully updated destination');
     res.redirect(`/destinations/${destination._id}`);

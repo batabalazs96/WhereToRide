e552ccd55db7f625f06c9c965468780ed2014f6b
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-07-18 14:06:08 +0200

    add a consolelog

--- app.js
+++ app.js
@@ -165,6 +165,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
+console.log("macsek")
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running...${port}`);

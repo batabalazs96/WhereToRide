b0b2d218276efb1371d575757d9a4a6242d88387
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-07-18 14:00:37 +0200

    add start scritp

--- app.js
+++ app.js
@@ -165,6 +165,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Server is running...');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running...${port}`);
 })
